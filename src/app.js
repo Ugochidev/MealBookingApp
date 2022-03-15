b403d51b13658e0988e9b01679726d54927f3304
Ugochidev <ugochukwuchioma16@gmail.com>
2022-03-15 11:21:30 +0100

    meal app

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// routes
+import mealRoutes from '../src/routes/meal.route'
+
+
+const app = express();
+const PORT = 9001;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    return res.send('The API is working');
+});
+// handle
+app.use('/api/v1/meals', mealRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
